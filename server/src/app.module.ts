021d5057cd37dc62518352188a56bee3b9197113
Khoa <khoanguyen.1881@gmail.com>
2025-10-06 09:02:30 +0700

    connect database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+        maxPoolSize: 20,
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000,
+        connectionFactory: (connection: Connection) => {
+          //- điều này giúp sử dụng soft delete
+          // connection.plugin(softDeletePlugin);
+          // return connection;
+        },
+      }),
+      inject: [ConfigService],
+    }),
+
+    //- configModule giúp sử dụng file .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
