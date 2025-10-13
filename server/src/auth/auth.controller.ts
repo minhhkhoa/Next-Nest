118388a09544715750b4b2eab9c1f60a44df8ad3
Khoa <khoanguyen.1881@gmail.com>
2025-10-13 17:31:20 +0700

    Update auth.controller.ts
    - Tiếp theo sẽ tạo form login để kiểm tra api.

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -11,7 +11,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
+  @Post('login')
   @ApiBody({ type: LoginDto })
   async login(@Request() req: any) {
     return this.authService.login(req.user);
