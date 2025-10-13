90f141d9872c4c2380575014f916de6d95816f0a
Khoa <khoanguyen.1881@gmail.com>
2025-10-13 11:10:33 +0700

    Update user.service.ts
    - Còn một vài api thì khi nào vẽ giao diện rồi viết tiếp.

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -6,12 +6,13 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import { BadRequestCustom } from 'src/customExceptions/BadRequestCustom';
 import { hashPassword } from 'src/utils/hashPassword';
+import mongoose from 'mongoose';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectModel(User.name)
-    private nuserModel: SoftDeleteModel<UserDocument>,
+    private userModel: SoftDeleteModel<UserDocument>,
   ) {}
   async create(createUserDto: CreateUserDto) {
     try {
@@ -20,7 +21,7 @@ export class UserService {
       if (password.length < 8)
         throw new BadRequestCustom('Mật khẩu phải có ít nhất 8 ký tự');
 
-      const user = await this.nuserModel.findOne({ email });
+      const user = await this.userModel.findOne({ email });
 
       if (user) throw new BadRequestCustom('Email này đã được đăng ký', !!user);
 
@@ -31,7 +32,7 @@ export class UserService {
         password: hashedPassword,
       };
 
-      const createdUser = await this.nuserModel.create(newUser);
+      const createdUser = await this.userModel.create(newUser);
 
       if (!createdUser)
         throw new BadRequestCustom('Tạo người dùng thất bại', !!createdUser);
@@ -45,19 +46,104 @@ export class UserService {
     }
   }
 
-  findAll() {
-    return `This action returns all user`;
+  //- sau thêm api getUser by filter name, email...
+
+  async findAll() {
+    try {
+      return this.userModel.find({ isDeleted: false }).populate({
+        path: 'companyID',
+        match: { isDeleted: false },
+        select: 'name _id',
+      });
+    } catch (error) {
+      throw new BadRequestCustom(error.message, !!error.message);
+    }
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new BadRequestCustom('ID user không đúng định dạng', !!id);
+      }
+
+      const user = await this.userModel.findById(id).populate([
+        {
+          path: 'companyID',
+          match: { isDeleted: false },
+          select: 'name _id',
+        },
+        // {
+        //   path: 'roleID',
+        //   match: { isDeleted: false },
+        //   select: 'name _id',
+        // },
+      ]);
+
+      if (!user) throw new BadRequestCustom('ID user không tìm thấy', !!id);
+
+      if (user?.isDeleted) {
+        throw new BadRequestCustom(
+          'user này hiện đã bị xóa',
+          !!user?.isDeleted,
+        );
+      }
+
+      return user;
+    } catch (error) {
+      throw new BadRequestCustom(error.message, !!error.message);
+    }
   }
 
-  update(id: string, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new BadRequestCustom('ID user không đúng định dạng', !!id);
+      }
+
+      const user = await this.userModel.findById(id);
+      if (!user) throw new BadRequestCustom('ID user không tìm thấy', !!id);
+
+      const filter = {
+        _id: id,
+      };
+
+      const update = {
+        $set: updateUserDto,
+      };
+
+      const result = await this.userModel.updateOne(filter, update);
+
+      if (result.modifiedCount === 0)
+        throw new BadRequestCustom('Lỗi sửa user', !!id);
+
+      return result;
+    } catch (error) {
+      throw new BadRequestCustom(error.message, !!error.message);
+    }
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new BadRequestCustom('ID user không đúng định dạng', !!id);
+      }
+
+      const user = await this.userModel.findById(id);
+      if (!user) throw new BadRequestCustom('ID user không tìm thấy', !!id);
+
+      const isDeleted = user.isDeleted;
+
+      if (isDeleted)
+        throw new BadRequestCustom('user này đã được xóa', !!isDeleted);
+
+      const filter = { _id: id };
+      const result = this.userModel.softDelete(filter);
+
+      if (!result) throw new BadRequestCustom('Lỗi xóa user', !!id);
+
+      return result;
+    } catch (error) {
+      throw new BadRequestCustom(error.message, !!error.message);
+    }
   }
 }
