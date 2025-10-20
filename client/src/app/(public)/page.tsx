53655108f4d52830058f18bd6b12fc6e738fb24b
Khoa <khoanguyen.1881@gmail.com>
2025-10-20 19:47:06 +0700

    fix bug build FE

--- client/src/app/(public)/page.tsx
+++ client/src/app/(public)/page.tsx
@@ -1,32 +1,10 @@
-"use client";
+import { Suspense } from "react";
+import Home from "../_pages/home/pageHome";
 
-import { Button } from "@/components/ui/button";
-import http from "@/lib/http";
-import { setAccessTokenToLocalStorage } from "@/lib/utils";
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-
-export default function Home() {
-  const searchParams = useSearchParams();
-  const access_token = searchParams.get("access_token");
-  const getPage = async () => {
-    const response = await http.get("auth/profile");
-    console.log("res: ", response);
-  };
-
-  useEffect(() => {
-    if (!access_token) return;
-
-    setAccessTokenToLocalStorage(access_token);
-  }, [access_token]);
-
-  useEffect(() => {
-    getPage();
-  }, []);
-
+export default function HomePage() {
   return (
-    <>
-      <Button>Click me!</Button>
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Home />
+    </Suspense>
   );
 }
