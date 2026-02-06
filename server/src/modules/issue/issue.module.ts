69cf779a8d765ae2cfae4caf73109388341907b3
Khoa <khoanguyen.1881@gmail.com>
2026-02-06 16:23:52 +0700

    Register Issue schema and add module imports
    - Sửa issue module, thêm các service cần thiết vào.

--- server/src/modules/issue/issue.module.ts
+++ server/src/modules/issue/issue.module.ts
@@ -1,8 +1,19 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { IssueService } from './issue.service';
 import { IssueController } from './issue.controller';
+import { UserModule } from '../user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Issue, IssueSchema } from './schemas/issue.schema';
+import { TranslationModule } from 'src/common/translation/translation.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
+  imports: [
+    TranslationModule,
+    MailModule,
+    forwardRef(() => UserModule),
+    MongooseModule.forFeature([{ name: Issue.name, schema: IssueSchema }]),
+  ],
   controllers: [IssueController],
   providers: [IssueService],
 })
